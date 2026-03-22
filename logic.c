13f016b0490d83702a2e5a81dca68e36211cc5c7
evilviper12 <abdelhakim0055@gmail.com>
2026-03-22 16:09:55 +0100

    9adit counter dial lbomb 50%

--- logic.c
+++ logic.c
@@ -20,21 +20,38 @@ t_board *board_create(int bomb_amount)
     }
     i++;
   }
-    srand(time(NULL));
-    i = 0;
-    while(i < temp->bomb_amount){
-      int x = rand() % COLS;
-      int y = rand() % ROWS;
-        temp->grid[x][y].is_bomb = true;
-      i++;
+  srand(time(NULL));
+  int k = 0;
+  i = 0;
+  while(k < temp->bomb_amount){
+    int x = rand() % COLS;
+    int y = rand() % ROWS;
+    temp->grid[x][y].is_bomb = true;
+    int x2, y2;
+    i = -1;
+    while(i <= 1){
+      j = -1;
+      while(j <= 1){
+        x2 = x + i;
+        y2 = y + j;
+        j++;
+        if(x2 == x&&y2 == y)
+          continue;
+        if(x2 < 0||x2 >= 12||y2 < 0||y2 >= 12)
+          continue;
+        temp->grid[x2][y2].bombs++;
       }
+      i++;
+    }
+    k++;
+  }
   return (temp);
 }
 void board_draw(t_board *board){
   int i;
   int j;
   i = 0;
-  
+
   printf("+-----------------------+\n");
   while(i < ROWS){
     j = 0;
